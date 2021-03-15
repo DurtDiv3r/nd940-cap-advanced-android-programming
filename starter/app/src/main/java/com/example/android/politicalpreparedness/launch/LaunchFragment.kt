a1d309132afcf24f8748d566dddedc30989c6151
DurtDiv3r <durtdev@gmail.com>
2021-03-15 14:22:34 +0200

    Commit: LaunchFragment and LaunchViewModel

--- starter/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -1,26 +1,43 @@
 package com.example.android.politicalpreparedness.launch
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
-import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
-import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
-import com.example.android.politicalpreparedness.election.adapter.ElectionListener
+import com.example.android.politicalpreparedness.election.ElectionsFragment
+import com.example.android.politicalpreparedness.representative.DetailFragment
 
 class LaunchFragment : Fragment() {
+
+    private lateinit var viewModel: LaunchViewModel
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
-
+        viewModel = ViewModelProvider(this).get(LaunchViewModel::class.java)
+        binding.viewModel = viewModel
         binding.representativeButton.setOnClickListener { navToRepresentatives() }
         binding.upcomingButton.setOnClickListener { navToElections() }
+
+        viewModel.navigateToFragment.observe(viewLifecycleOwner) {
+            when (it) {
+                ElectionsFragment::class.java.simpleName -> {
+                    navToElections()
+                    viewModel.navigateToFragmentDone()
+                }
+                DetailFragment::class.java.simpleName -> {
+                    navToRepresentatives()
+                    viewModel.navigateToFragmentDone()
+                }
+            }
+        }
 
         return binding.root
     }
